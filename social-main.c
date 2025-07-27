26255b328689a11f9b0c0b57c222fab22b0f4a8a
Jenrick Lim <jenrick_lim@dlsu.edu.ph>
2025-07-27 23:20:04 +0800

    Updated social-main.c

--- social-main.c
+++ social-main.c
@@ -13,7 +13,7 @@ int main()
 //	printf("Loaded %ld nodes and %ld edges\n", socialMedia.nodes, socialMedia.edges);
 //	
 //	for (int i = 0; i < 10 && i < socialMedia.nodes; i++) { // show only first 10
-//	    printf("ID: %08d, Name: %s %s, Friends: %d\n",
+//	    printf("ID: %d, Name: %s %s, Friends: %d\n",
 //	           socialMedia.userAccount[i].ID,
 //	           socialMedia.userAccount[i].userName.fName,
 //	           socialMedia.userAccount[i].userName.lName,
